aa72f8a54bb8e238e959829b5fbb5fd260543ba2
Byron Austin <byronaustin007@gmail.com>
2013-01-14 09:04:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeeHive::Application.config.secret_token = 'c6bd68af764cea01c6d05ce8325a50eb673fe01003d84bf599209b91a544b2668236171fc956dd44bc76ea310a5e760748a187ff3b766b33b3b6cbdece12c4ca'
